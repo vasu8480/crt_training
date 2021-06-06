fee17e55cf116b101fb4b70614d59f7b5dd458a5
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2021-06-06 12:36:43 +0530

    Create replacemin.c

---
+++ day5/replacemin.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void replacemin(int *arr,int size)
+{
+	int i,min=arr[0];
+	for(i=1;i<size;i++)
+	{
+		if(min>arr[i])
+		{
+			min=arr[i];
+		}
+	}
+	for(i=0;i<size;i++)
+	{
+		arr[i]=min;
+	}
+}
+int main()
+{
+	int arr[100],i,size;
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	replacemin(arr,size);
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
