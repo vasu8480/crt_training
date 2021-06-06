df6c008e464d3ae31d1b3a8264ea584d58f15cc4
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2021-06-06 12:36:45 +0530

    Create replacewithfirstodd.c

---
+++ day5/replacewithfirstodd.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void replacewithfirstodd(int *arr,int size)
+{
+	int i,odd;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2)
+		{
+			odd=arr[i];
+			break;
+			
+		}
+	}
+	
+	for(i=0;i<size;i++)
+	{
+		if(arr[i]%2<=arr[i])
+			arr[i]=odd;
+	}
+}
+int main()
+{
+	int arr[100],i,size;
+	scanf("%d",&size);
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	replacewithfirstodd(arr,size);
+	for(i=0;i<size;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
