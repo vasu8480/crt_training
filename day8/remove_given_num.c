4c8f93a2497dbf373940b6b9c4b4da55a21b3b0b
Vasu Nurukurthi <48316372+vasu8480@users.noreply.github.com>
2021-06-09 21:35:57 +0530

    Create remove_given_num.c

---
+++ day8/remove_given_num.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void removeelement(int *arr,int size,int k)
+{
+	int i,j;
+	for (i=0; i<size-1; i++)
+    if (arr[i] == k)
+        break;
+	if (i < size)
+	{
+    	size = size - 1;
+    	for (j=i; j<size; j++)
+        	arr[j] = arr[j+1];
+	}
+	return ;
+}
+int main()
+{
+	int arr[100],n,i,k;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	scanf("%d",&k);
+	removeelement(arr,n,k);
+	for(i=0;i<n-1;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
