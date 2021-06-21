4f720024d1f15d4f808fcb772b43d1e201eba7d1
vasu8480 <vasu8480@gmail.com>
2021-06-21 21:44:01 +0530

    Create max_ele_in array.c

---
+++ day16/max_ele_in array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+  int m, n, c, d, matrix[100][100], maximum;
+  scanf("%d%d",&m,&n);
+
+  for (c = 0; c < m; c++)
+    for(d = 0; d < n; d++)
+      scanf("%d", &matrix[c][d]);
+
+  maximum = matrix[0][0];
+
+  for (c = 0; c < m; c++)
+    for (d = 0; d < n; d++)
+      if (matrix[c][d] > maximum)
+        maximum = matrix[c][d];
+  printf("%d",maximum);
+
+  return 0;
+}
