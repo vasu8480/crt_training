21ed553bf6eed25851e5088253109e53449e7a12
vasu8480 <vasu8480@gmail.com>
2021-07-15 15:44:49 +0530

    Create count_of_occurrences.c

---
+++ 2,3-7-2021/count_of_occurrences.c
@@ -0,0 +1,29 @@
+//find number of occurrences of a given element.
+#include<stdio.h>
+int countOccurance(int len,int value,int *arr)
+{
+	int i=0,count =0;
+	while(i<len)
+	{
+		if(arr[i] == value)
+			count += 1;
+		i+=1;
+	}
+	return count;
+}
+int main()
+{
+	int i,arr[100],len,count,val;
+	scanf("%d",&len);
+	scanf("%d",&val);
+	for(i=0;i<len;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	count=countOccurance(len,val,arr);
+	
+	printf("%d ",count);
+	
+}
+
